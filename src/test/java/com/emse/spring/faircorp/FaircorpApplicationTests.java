1338bd0374672c0ac21e972ad0854c30441b9f73
Nushrat JAHAN <nushrat.jahan@etu.emse.fr>
2022-12-30 20:20:45 +0100

    Added checkstyle

--- src/test/java/com/emse/spring/faircorp/FaircorpApplicationTests.java
+++ src/test/java/com/emse/spring/faircorp/FaircorpApplicationTests.java
@@ -3,9 +3,11 @@ package com.emse.spring.faircorp;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
+@SuppressWarnings("checkstyle:MissingJavadocType")
 @SpringBootTest
-public class FaircorpApplicationTests {
+class FaircorpApplicationTests {
 
+	@SuppressWarnings("checkstyle:Indentation")
 	@Test
 	public void contextLoads() {
 	}
